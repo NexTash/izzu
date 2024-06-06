a26cd7941cc2ea92fc6c4d8535bab821153a5df5
alibajwa007 <babargoraya878@gmail.com>
2024-06-06 15:22:07 +0500

    fixes

---
+++ izzu/event/orderreq.py
@@ -0,0 +1,23 @@
+import frappe
+import json
+from frappe import _
+
+@frappe.whitelist()
+def myfunc(formData):
+    data = json.loads(formData)
+    # frappe.msgprint(f"{data['name']}")
+    doc = frappe.new_doc("Order Request")
+    frappe.msgprint(f"{data['bouquet_price']}")
+    
+
+    doc.name1 =  data['name']
+    doc.phone = data['phone']
+    doc.email = data['email']
+    doc.address = data['address']
+    doc.bouquet_type = data['bouquet_type']
+    doc.gift_wrapping = data['gift_wrapping']
+    doc.urgent_delivery = data['urgent_delivery']
+    doc.bouquet_price = data['bouquet_price']
+    doc.insert()
+    # doc.save()
+    
