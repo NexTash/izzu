2bd1ec38f8b6c502ff650bb17c42f6ee1607f6d1
alibajwa007 <babargoraya878@gmail.com>
2024-06-07 15:58:37 +0500

    fixes

--- izzu/izzu/doctype/student/student.js
+++ izzu/izzu/doctype/student/student.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Student', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        frm.add_custom_button(__('Send Email'), function() {
+            // frappe.msgprint(__('Custom Button Clicked!'));
+            
+            frm.call({
+                method: 'izzu.izzu.doctype.student.student.send_data',
+                args: {
+                    // doc: frm.doc  
+                },
+            });
+        });
+    }
 });
+
--- izzu/izzu/doctype/student/student.py
+++ izzu/izzu/doctype/student/student.py
@@ -1,9 +1,30 @@
-import json
+# import json
 import frappe
 from frappe.model.document import Document
 
 class Student(Document):
 	pass
+@frappe.whitelist()
+def	send_data():
+	email = "itxjutt008@gmail.com"
+	sender = frappe.session.user
+	subject = "Subject of the email"
+	message = "This is the body of the email."
+	frappe.sendmail(
+	recipients=email,
+	sender=None,
+	subject=subject,
+	message=message,
+	header=[subject, "green"],
+	delayed= False,
+	retry=3,
+)
+
+
+
+
+
+
 # @frappe.whitelist()
 # def my_function(docname):
 # 		stu_child = []
@@ -13,15 +34,15 @@ class Student(Document):
 # 		return stu_child
 
 
-@frappe.whitelist()
-def my_function(docname):
-	doc = frappe.get_doc("Student", docname)
-	for row in doc.stu_child:
+# @frappe.whitelist()
+# def my_function(docname):
+# 	doc = frappe.get_doc("Student", docname)
+# 	for row in doc.stu_child:
 
-		new_doc = frappe.new_doc('Student2')
-		new_doc.name1 = row.name1
-		new_doc.class1 = row.class1
-		new_doc.address = row.address
+# 		new_doc = frappe.new_doc('Student2')
+# 		new_doc.name1 = row.name1
+# 		new_doc.class1 = row.class1
+# 		new_doc.address = row.address
 	
 	
-		new_doc.insert()
+# 		new_doc.insert()
